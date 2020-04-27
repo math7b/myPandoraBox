035dca09dfa2eebd53f6fdfa2050a1a543d7b362
Hiragy7 <hiragi.barth@gmail.com>
2020-04-27 16:37:25 -0300

    My second Hello World with React.js 'Font-end'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! cheguei na bagaça.
-        </p>
-      </header>
-    </div>
+    <p>
+      Hello World! cheguei na bagaça.
+    </p>
   );
 }
 
